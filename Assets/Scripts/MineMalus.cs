d2eba7e967cf73352dd521d97703ef01bda9c1e5
Arthur Manoux <arthur.manoux@wanadoo.fr>
2017-11-21 10:12:38 +0100

    assemblages modif scripts prefab modé

--- Assets/Scripts/MineMalus.cs
+++ Assets/Scripts/MineMalus.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MineMalus : Bonus
+public class MineMalus : Bonus , ITakeDamage
 {
     public float Damage = 10f;
+    public float MaxHelath = 100f;
+
+    private float _currentHealth = 0f;
 
     public override void ApplyBonus(Player player)
     {
@@ -12,4 +15,25 @@ public class MineMalus : Bonus
         Destroy(gameObject);
     }
 
+
+    // Use this for initialization
+    void Start()
+    {
+        _currentHealth = MaxHelath;
+    }
+
+    public void TakeDamage(float damage, GameObject instigator)
+    {
+        _currentHealth -= damage;
+        if (_currentHealth <= 0f)
+        {
+            Kill();
+        }
+
+    }
+
+    public void Kill()
+    {
+        Destroy(this.gameObject);
+    }
 }
