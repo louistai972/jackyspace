f0a0a9e9e8e070c768c1881a3b2f4d8f30df9612
Arthur Manoux <arthur.manoux@wanadoo.fr>
2017-11-22 13:07:03 +0100

    vfx / main scene

--- Assets/Scripts/DestructibleObstacle.cs
+++ Assets/Scripts/DestructibleObstacle.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 public class DestructibleObstacle : MonoBehaviour, ITakeDamage
 {
     public float MaxHelath = 100f;
+    public ParticleSystem Boom;
+    private bool death= false;
 
     private float _currentHealth = 0f;
 
 	// Use this for initialization
 	void Start ()
     {
+        Boom.Play();
         _currentHealth = MaxHelath;
 	}
+
+    void Explosions()
+    {
+        Boom.Play();
+    }
+
 
     public void TakeDamage(float damage, GameObject instigator)
     {
         _currentHealth -= damage;
         if (_currentHealth <= 0f)
         {
+            Explosions();
             Kill();
         }
 
