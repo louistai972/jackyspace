d4b72e1c8f67cd0d3d2d40c134fc5ba2c05712bb
MSI\louis <lou_iis@live.fr>
2017-11-24 12:45:35 +0100

    Menu ok

---
+++ Assets/Scripts/BoutonMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BoutonMenu : MonoBehaviour {
+
+    public string TestLD;
+
+    public void ButtonPlay()
+    {
+        SceneManager.LoadScene(TestLD);
+    }
+    public void ButtonExit()
+    {
+        Application.Quit();
+    }
+}
